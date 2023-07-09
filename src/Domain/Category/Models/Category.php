75ef38e8da33665e730bce8fc41810935e7f0ec3
oberdiyewa <ezkahan.dev@gmail.com>
2023-07-10 00:53:29 +0500

    category model

--- src/Domain/Category/Models/Category.php
+++ src/Domain/Category/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace Domain\Category\Models;
 
+use Domain\Product\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
