dfefe7ec4898884a188eee16d33bbab3919af1e0
oberdiyewa <ezkahan.dev@gmail.com>
2023-07-13 17:09:16 +0500

    cart bugfix

--- src/Domain/Cart/Models/Cart.php
+++ src/Domain/Cart/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace Domain\Cart\Models;
 
+use Domain\User\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
