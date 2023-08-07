30fedf945c43208d093845426f8892f590bbb389
ezkahan <ezkahan.dev@gmail.com>
2023-08-07 18:31:24 +0500

    subcategories paginate

--- src/App/Http/GraphQL/Category/Queries/GetSubCategoriesQuery.php
+++ src/App/Http/GraphQL/Category/Queries/GetSubCategoriesQuery.php
@@ -12,6 +12,6 @@ final class GetSubCategoriesQuery
      */
     public function __invoke($_, array $args)
     {
-        return Category::subCategories()->paginate();
+        return Category::subCategories()->paginate(24);
     }
 }
